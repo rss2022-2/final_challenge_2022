1e481a5645cff31a485f03b144f7a8e5608ffc5f
akilasar <77478803+akilasar@users.noreply.github.com>
2022-04-27 15:51:24 -0400

    Update stop_detector.py

--- city_driving/stop_detector/stop_detector.py
+++ city_driving/stop_detector/stop_detector.py
@@ -4,20 +4,26 @@ import rospy
 import numpy as np
 from sensor_msgs.msg import Image
 from detector import StopSignDetector
+from std_msgs.msg import Float32
 
 class SignDetector:
     def __init__(self):
-        self.detector = StopSignDetector()
-        self.publisher = None #TODO
+        self.detector = StopSignDetector(threshold=0)
+        self.publisher = rospy.Publisher("/stop_sign_distance", Float32, queue_size=10)
         self.subscriber = rospy.Subscriber("/zed/zed_node/rgb/image_rect_color", Image, self.callback)
+        self.bbox = [0, 0, 0, 0]
 
     def callback(self, img_msg):
-        # Process image without CV Bridge
         np_img = np.frombuffer(img_msg.data, dtype=np.uint8).reshape(img_msg.height, img_msg.width, -1)
         bgr_img = np_img[:,:,:-1]
         rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
 
-        #TODO: 
+        present, bbox = self.detector.predict(rgb_img)
+        if not present:
+            self.bbox = [0, 0, 0, 0]
+            self.publisher.publish(100)
+        else:
+            self.bbox = bbox
 
 if __name__=="__main__":
     rospy.init_node("stop_sign_detector")
