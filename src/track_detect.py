fffa6d24d2c95d375439fbf59a33c0478c6685fe
Quang Kieu <quang.p.kieu@gmail.com>
2022-04-28 13:54:55 -0400

    Start simple detect

--- src/track_detect.py
+++ src/track_detect.py
@@ -7,7 +7,7 @@ import cv2 as cv
 from cv_bridge import CvBridge, CvBridgeError
 
 from sensor_msgs.msg import Image
-from final_challenge_2022.msg import TrackLane
+from final_challenge_2022.msg import Lookahead
 from homography_transformer import HomographyTransformer
 
 LEFT_COLOR = (184, 87, 0)
@@ -28,24 +28,29 @@ class TrackDetector():
         self.upper_bound = np.array(rospy.get_param("~color_upper_bound"), np.uint8)
         self.pt_left = rospy.get_param("~pt_left")
         self.pt_right = rospy.get_param("~pt_right")
+        max_lookahead = rospy.get_param("~max_lookahead")
+        self.homography_transformer = HomographyTransformer(pts_image_plane, pts_ground_plane)
+        self.pt_left_uv = self.homography_transformer.transform_xy_to_uv(self.pt_left)
+        self.pt_right_uv = self.homography_transformer.transform_xy_to_uv(self.pt_right)
+        self.max_lookahead_uv = self.homography_transformer.transform_xy_to_uv((max_lookahead, 0))
         self.lp_factor = rospy.get_param("~lp_factor")
 
-        self.track_msg = TrackLane()
+        self.lookahead_msg = Lookahead()
 
         # Subscribe to ZED camera RGB frames
         if self.send_debug:
             rospy.loginfo("Track detector with debug")
-            self.debug_pub = rospy.Publisher(debug_topic, Image, queue_size=10)
-        self.track_pub = rospy.Publisher(track_topic, TrackLane, queue_size=10)
+            self.debug_pub = rospy.Publisher(debug_topic, Image, queue_size=1)
+        self.track_pub = rospy.Publisher(track_topic, Lookahead, queue_size=1)
         self.image_sub = rospy.Subscriber(image_topic, Image, self.image_callback)
         self.bridge = CvBridge() # Converts between ROS images and OpenCV Images
-        self.homography_transformer = HomographyTransformer(pts_image_plane, pts_ground_plane)
 
     def image_callback(self, image_msg):
         image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
         height, width, channels = image.shape
         cv.rectangle(image, (0,height), (width, height-150), (0,0,0), -1)
-        
+        max_lookahead_line = [(0, self.max_lookahead_uv[1]),(width, self.max_lookahead_uv[1])]
+
         lines, mask = TrackDetector.__get_hough_lines(image, self.lower_bound, self.upper_bound, cv.COLOR_BGR2HLS)
 
         # if self.send_debug:
@@ -53,39 +58,46 @@ class TrackDetector():
 
         left_line, right_line = self.__get_track(lines, image)
 
-        track_msg = TrackLane()
-
+        left_lookahead_intersect = None
+        right_lookahead_intersect = None
+        left_right_intersect = None
+        intersect_u = None
+        intersect_v = None
         if left_line is not None:
-            slope_left, intercept_left = TrackDetector.__get_slope_intercept(left_line)
-            self.track_msg.slope_left = TrackDetector.__lp(self.track_msg.slope_left, slope_left, self.lp_factor)
-            self.track_msg.intercept_left = TrackDetector.__lp(self.track_msg.intercept_left, intercept_left, self.lp_factor)
-            track_msg.slope_left = self.track_msg.slope_left
-            track_msg.intercept_left = self.track_msg.intercept_left
+            left_lookahead_intersect = self.__get_intersect(left_line, max_lookahead_line)
+            intersect_u, intersect_v = left_lookahead_intersect
             if self.send_debug:
-                #rospy.loginfo("Left line detected!")
-                self.__draw_xy_line(left_line, image, LEFT_COLOR)
-                self.__draw_xy_point(self.pt_left, image, LEFT_COLOR)
-        else:
-            track_msg.slope_left, track_msg.intercept_left = [float("NaN"), float("NaN")]
-        
+                TrackDetector.__draw_point((intersect_u, intersect_v), image, LEFT_COLOR)
+                TrackDetector.__draw_line(left_line, image, LEFT_COLOR)
         if right_line is not None:
-            slope_right, intercept_right = TrackDetector.__get_slope_intercept(right_line)
-            self.track_msg.slope_right = TrackDetector.__lp(self.track_msg.slope_right, slope_right, self.lp_factor)
-            self.track_msg.intercept_right = TrackDetector.__lp(self.track_msg.intercept_right, intercept_right, self.lp_factor)
-            track_msg.slope_right = self.track_msg.slope_right
-            track_msg.intercept_right = self.track_msg.intercept_right
+            right_lookahead_intersect = self.__get_intersect(right_line, max_lookahead_line)
+            intersect_u, intersect_v = right_lookahead_intersect
             if self.send_debug:
-                #rospy.loginfo("Right line detected!")
-                self.__draw_xy_line(right_line, image, RIGHT_COLOR)
-                self.__draw_xy_point(self.pt_right, image, RIGHT_COLOR)
-        else:
-            track_msg.slope_right, track_msg.intercept_right = [float("NaN"), float("NaN")]
+                TrackDetector.__draw_point((intersect_u, intersect_v), image, RIGHT_COLOR)
+                TrackDetector.__draw_line(right_line, image, RIGHT_COLOR)
+        if left_line and right_line is not None:
+            left_right_intersect = self.__get_intersect(right_line, left_line)
+            if left_right_intersect[1] < left_lookahead_intersect[1]:
+                intersect_u, intersect_v = left_right_intersect
+            else:
+                intersect_u = (left_lookahead_intersect[0] + right_lookahead_intersect[0])/2
+                intersect_v = self.max_lookahead_uv[1]
+
+        if intersect_u is not None:
+            x, y = self.homography_transformer.transform_uv_to_xy((intersect_u, intersect_v))
+            self.lookahead_msg.x = TrackDetector.__lp(self.lookahead_msg.x, x, self.lp_factor)
+            self.lookahead_msg.y = TrackDetector.__lp(self.lookahead_msg.y, y, self.lp_factor)
+            if self.send_debug:
+                TrackDetector.__draw_point((intersect_u, intersect_v), image, (255,0,255))
 
         if self.send_debug:
+            TrackDetector.__draw_point(self.pt_left_uv, image, LEFT_COLOR)
+            TrackDetector.__draw_point(self.pt_right_uv, image, RIGHT_COLOR)
+            TrackDetector.__draw_line(max_lookahead_line, image, (0,0,255))
             debug_msg = self.bridge.cv2_to_imgmsg(image, "bgr8")
             self.debug_pub.publish(debug_msg)
 
-        self.track_pub.publish(track_msg)
+        self.track_pub.publish(self.lookahead_msg)
     
     @staticmethod
     def __lp(value, new_value, factor):
@@ -98,22 +110,30 @@ class TrackDetector():
         intercept = line[0][1] - line[0][0]*slope
         return slope, intercept
 
+    @classmethod
+    def __get_intersect(cls, line1, line2):
+        slope_1, intercept_1 = cls.__get_slope_intercept(line1)
+        slope_2, intercept_2 = cls.__get_slope_intercept(line2)
+        u = (intercept_2-intercept_1)/(slope_1-slope_2)
+        v = u*slope_1 + intercept_1
+        return u, v
 
-    def __draw_xy_point(self, point, image, color):
-        p_uv = self.homography_transformer.transform_xy_to_uv(point)
-        cv.circle(image, tuple(p_uv), 10, color, -1)
-
-    def __draw_xy_line(self, line, image, color):
-        p1_uv = self.homography_transformer.transform_xy_to_uv(line[0])
-        p2_uv = self.homography_transformer.transform_xy_to_uv(line[1])
-        cv.line(image, tuple(p1_uv), tuple(p2_uv), color, 5, cv.LINE_AA)
+    @staticmethod
+    def __draw_point(point, image, color):
+        point = np.array(point, dtype=int)
+        cv.circle(image, tuple(point), 4, color, -1)
+    
+    @staticmethod
+    def __draw_line(line, image, color):
+        line = np.array(line, dtype=int)
+        cv.line(image, tuple(line[0]), tuple(line[1]), color, 2, cv.LINE_AA)
 
     @staticmethod
     def __track_update(line, p, best_line, best_dist):
         p1_xy, p2_xy = line
         new_dist = TrackDetector.__min_distance(p1_xy, p2_xy, p)
         # rospy.loginfo("dist: %f" % new_dist)
-        if best_dist is None or (new_dist < best_dist and new_dist < 1.0):
+        if best_dist is None or (new_dist < best_dist):
             return line, new_dist
         return best_line, best_dist
     
@@ -128,29 +148,27 @@ class TrackDetector():
             colors = [(0,0,255), (0,255,0), (255,0,0)]
             j = 0
             for line in lines:
-                p1_u, p1_v, p2_u, p2_v = line[0]
-                p1_xy = self.homography_transformer.transform_uv_to_xy([p1_u, p1_v])
-                p2_xy = self.homography_transformer.transform_uv_to_xy([p2_u, p2_v])
-                delta = (np.array(p2_xy) - np.array(p1_xy))
+                p1, p2 = np.reshape(line[0], (2,2))
+                delta = (np.array(p2) - np.array(p1))
                 slope = delta[1]/delta[0]
-                intercept = p1_xy[1] - p1_xy[0]*slope
-                line = [p1_xy, p2_xy]
-                if np.abs(slope) < 0.7:
-                    left_y = self.pt_left[0]*slope + intercept
-                    if left_y > self.pt_left[1]:
+                intercept = p1[1] - p1[0]*slope
+                line = [p1, p2]
+                if np.abs(slope) > 0.01:
+                    left_x = (self.pt_left_uv[1]-intercept)/slope
+                    if left_x > self.pt_left_uv[0]:
                         # rospy.loginfo("slope: %f" % slope)
                         best_line_left, best_dist_left = TrackDetector.__track_update(line, 
-                                                                                      self.pt_left, 
+                                                                                      self.pt_left_uv, 
                                                                                       best_line_left, 
                                                                                       best_dist_left)
-                    right_y = self.pt_right[0]*slope + intercept
-                    if right_y < self.pt_right[1]:
+                    right_x = (self.pt_right_uv[1]-intercept)/slope
+                    if right_x < self.pt_right_uv[0]:
                         best_line_right, best_dist_right = TrackDetector.__track_update(line, 
-                                                                                        self.pt_right, 
+                                                                                        self.pt_right_uv, 
                                                                                         best_line_right, 
                                                                                         best_dist_right)
                 if self.send_debug:
-                    cv.line(image, (p1_u, p1_v), (p2_u, p2_v), colors[j%3], 3, cv.LINE_AA)
+                    cv.line(image, p1, p2, colors[j%3], 3, cv.LINE_AA)
                     j += 1
         # rospy.loginfo("best: %f" % best_dist_left)
         # rospy.loginfo("-----------")
--- src/track_pursuit.py
+++ src/track_pursuit.py
@@ -11,7 +11,7 @@ from ackermann_msgs.msg import AckermannDriveStamped
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 from visualization_tools import *
-from final_challenge_2022.msg import TrackLane
+from final_challenge_2022.msg import Lookahead
 
 class TrackPursuit(object):
     """ Implements Pure Pursuit trajectory tracking with a fixed lookahead and speed.
@@ -43,7 +43,7 @@ class TrackPursuit(object):
         self.line_pub = rospy.Publisher(self.visual_topic, Marker, queue_size=1)
 
         #TODO: fill details
-        self.track_sub = rospy.Subscriber(self.track_topic, TrackLane, self.track_callback, queue_size=10)
+        self.track_sub = rospy.Subscriber(self.track_topic, Lookahead, self.track_callback, queue_size=10)
 
         self.point_car          = np.array([0, 0])
         self.car_unit_vec       = np.array([1, 0])
@@ -55,81 +55,84 @@ class TrackPursuit(object):
         self.drive_pub.publish(self.drive_msg)
 
     def track_callback(self, track_msg):
-        ''' Get 2 lines represent the track lane
-        '''
-        m_1    = track_msg.slope_left
-        b_1    = track_msg.intercept_left
-        m_2    = track_msg.slope_right
-        b_2    = track_msg.intercept_right
-        # assert m_1 != m_2, "2 lines are parallel, cannot happen"
+        # ''' Get 2 lines represent the track lane
+        # '''
+        # m_1    = track_msg.slope_left
+        # b_1    = track_msg.intercept_left
+        # m_2    = track_msg.slope_right
+        # b_2    = track_msg.intercept_right
+        # # assert m_1 != m_2, "2 lines are parallel, cannot happen"
 
-        if self.draw_lines:
-            self.__draw_line(m_1, b_1, self.line_pub)
-            self.__draw_line(m_2, b_2, self.line_pub)
+        # if self.draw_lines:
+        #     self.__draw_line(m_1, b_1, self.line_pub)
+        #     self.__draw_line(m_2, b_2, self.line_pub)
 
         # find intersection of 2 line
         # x = (b_2 - b_1) / (m_1 - m_2)
         # y = m_1*x + b_1
         # lookahead = np.array([x, y])
 
-        x = self.front_point
-        if not np.isnan(m_1) and not np.isnan(m_2):
-            side = 1
-            # y = (b_1 + b_2) / 2.0
-            distance_error = abs(b_1 - self.left_cam_offset) - self.half_track_width
-            angle_error = np.arctan(m_1) if m_1 != 0 else 0
-            rospy.loginfo("See both")
-            rospy.loginfo([m_1, b_1])
-            rospy.loginfo("distance error")
-            rospy.loginfo(distance_error)
-            rospy.loginfo("angel error")
-            rospy.loginfo(angle_error)
-        elif not np.isnan(m_1):
-            side = 1
-            # y = b_1 - self.half_track_width
-            distance_error = abs(b_1 - self.left_cam_offset) - self.half_track_width
-            angle_error = np.arctan(m_1) if m_1 != 0 else 0
-            rospy.loginfo("See only left")
-            rospy.loginfo([m_1, b_1])
-        elif not np.isnan(m_2):
-            side = -1
-            # y = self.half_track_width + b_2
-            distance_error = self.half_track_width - abs(b_2 + self.left_cam_offset)
-            angle_error = np.arctan(m_2) if m_2 != 0 else 0
-            rospy.loginfo("See only right")
-            rospy.loginfo([m_2, b_2])
-        else:
-            rospy.logerr("did not get any track lines")
-            return
+        # x = self.front_point
+        # if not np.isnan(m_1) and not np.isnan(m_2):
+        #     side = 1
+        #     # y = (b_1 + b_2) / 2.0
+        #     distance_error = abs(b_1 - self.left_cam_offset) - self.half_track_width
+        #     angle_error = np.arctan(m_1) if m_1 != 0 else 0
+        #     rospy.loginfo("See both")
+        #     rospy.loginfo([m_1, b_1])
+        #     rospy.loginfo("distance error")
+        #     rospy.loginfo(distance_error)
+        #     rospy.loginfo("angel error")
+        #     rospy.loginfo(angle_error)
+        # elif not np.isnan(m_1):
+        #     side = 1
+        #     # y = b_1 - self.half_track_width
+        #     distance_error = abs(b_1 - self.left_cam_offset) - self.half_track_width
+        #     angle_error = np.arctan(m_1) if m_1 != 0 else 0
+        #     rospy.loginfo("See only left")
+        #     rospy.loginfo([m_1, b_1])
+        # elif not np.isnan(m_2):
+        #     side = -1
+        #     # y = self.half_track_width + b_2
+        #     distance_error = self.half_track_width - abs(b_2 + self.left_cam_offset)
+        #     angle_error = np.arctan(m_2) if m_2 != 0 else 0
+        #     rospy.loginfo("See only right")
+        #     rospy.loginfo([m_2, b_2])
+        # else:
+        #     rospy.logerr("did not get any track lines")
+        #     return
         
-        total_error = angle_error*self.WEIGHT_ANGLE + distance_error*self.WEIGHT_DISTANCE
+        # total_error = angle_error*self.WEIGHT_ANGLE + distance_error*self.WEIGHT_DISTANCE
 
-        pid = total_error * self.GAIN_P
+        # pid = total_error * self.GAIN_P
 
-        # lookahead = np.array([x, y])
-
-        # ## find distance between car and lookahead
-        # lookahead_vec = lookahead - self.point_car
-        # distance = np.linalg.norm(lookahead_vec)
+        x = track_msg.x
+        y = track_msg.y
         
-        # ## find alpha: angle of the car to lookahead point
-        # lookahead_unit_vec = lookahead_vec / distance
-        # dot_product = np.dot(self.car_unit_vec, lookahead_unit_vec)
-        # dot_product = max(-1, dot_product) if dot_product < 0 else min(1, dot_product)
-        # assert -1 <= dot_product <= 1, dot_product
-        # alpha = np.arccos(dot_product)
+        lookahead = np.array([x, y])
 
-        # # steering angle
-        # steer_ang = np.arctan(2*self.wheelbase_length*np.sin(alpha)
-        #                 / (distance))
-        # steer_ang = steer_ang if y >= 0 else -steer_ang
+        ## find distance between car and lookahead
+        lookahead_vec = lookahead - self.point_car
+        distance = np.linalg.norm(lookahead_vec)
+
+        ## find alpha: angle of the car to lookahead point
+        lookahead_unit_vec = lookahead_vec / distance
+        dot_product = np.dot(self.car_unit_vec, lookahead_unit_vec)
+        dot_product = max(-1, dot_product) if dot_product < 0 else min(1, dot_product)
+        assert -1 <= dot_product <= 1, dot_product
+        alpha = np.arccos(dot_product)
+
+        # steering angle
+        steer_ang = np.arctan(2*self.wheelbase_length*np.sin(alpha)
+                        / (distance))
+        steer_ang = steer_ang if y >= 0 else -steer_ang
 
         # publish drive commands
         self.drive_msg = AckermannDriveStamped()
         # optimization: run fast if steer_ang is small
         # self.drive_msg.drive.speed = self.fast_speed if abs(steer_ang) <= self.small_angle else self.speed
         self.drive_msg.drive.speed = self.speed
-        self.drive_msg.drive.steering_angle = pid if (-0.34 <= pid <= 0.34) else -0.34 if pid <= 0 else 0.34
+        self.drive_msg.drive.steering_angle = steer_ang
         
     @staticmethod
     def __draw_line(slope, y_intercept, publisher, frame = "/base_link"):
